614e74ea7d88a2d8cd19ba23254b6059e4e16059
aR1be1r0 <berto.arthur@escola.pr.gov.br>
2025-08-13 14:11:00 +0000

    hfdgfgh

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Quem venceu a Copa do Mundo de 2018?',
+    'A França venceu a Copa do Mundo de 2018'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual time tem mais títulos da Liga dos Campeões da UEFA?',
+    'O Real Madrid tem mais títulos da Liga dos Campeões da UEFA'
+)
+
+criaCartao(
+    'Futebol',
+    'O que é um gol contra?',
+    'Um gol contra é quando um jogador marca acidentalmente um gol em sua própria meta'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem é o maior artilheiro da história da seleção brasileira?',
+    'O maior artilheiro da história da seleção brasileira é Pelé'
+)
